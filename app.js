1096af6630f97db674e0532c8c66b15497a36cb9
Naman Aggarwal <naman.aggarwal2001@gmail.com>
2021-04-06 17:01:24 +0530

    complete

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const cors = require("cors");
+
+app.use(cors());
+io.on("connection", (socket) => {
+  console.log("User Online");
+
+  socket.on("canvas-data", (data) => {
+    socket.broadcast.emit("canvas-data", data);
+  });
+});
+
+const PORT =process.env.PORT || 5000;
+http.listen(PORT, () => {
+  console.log("server started on ", PORT);
+});
